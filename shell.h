02d68c59c1676ae30cb5ece1bceeb6ca1a7f9741
hegavon <victorgavon@gmail.com>
2023-11-14 14:07:12 -0800

    Shell header file update

--- shell.h
+++ shell.h
@@ -34,3 +34,80 @@
 
 #define FILE_HISTORY	".simple_shell_history"
 #define MAXIMUM_HISTORY	4096
+
+extern char **environ;
+
+
+/**
+ * struct liststr - The list structure
+ * @num: The number of the node
+ * @str: The string
+ * @next: This is the next node in the list
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ *struct passinfo - This contains the info for the pass command
+ *		and allows uniform prototypes for function pointer struct
+ *@arg: This is the string generated from getline containing arguements
+ *@argv: This is an array of strings generated from arg
+ *@path: The string path for the current command
+ *@argc: An argument count
+ *@lineCount: This is the error line count
+ *@errorNumber: This is the error code for exit()s
+ *@lineCountFlag: The line count flag
+ *@fileName: This is the filename for the current program
+ *@env: This is the environment list
+ *@environ: The custom modified environment list
+ *@history: Tis is the history list
+ *@alias: The alias list
+ *@env_changed: This is the environment changed flag
+ *@status: This is the return status of the last exec'd command
+ *@commandBuffer: The address of pointer to commandBuffer, on if chaining
+ *@commandBufferType: CMD_type ||, &&, ;
+ *@input_fd: The file descriptor from which to get line input
+ *@historyCount: The history count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int lineCount;
+	int errorNumber;
+	int lineCountFlag;
+	char *fileName;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **commandBuffer;
+	int commandBufferType;
+	int input_fd;
+	int historyCount;
+} ShellContext;
+
+#define INITIALIZE_INFO_STRUCT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
+
+/**
+ * @struct BuiltinEntry - Represents an entry in the built-in commands table
+ * @type: The type of built-in command
+ * @function: The function pointer to the built-in command handler
+ */
+typedef struct BuiltinEntry
+{
+	char *type;
+	int (*function)(ShellContext *);
+} BuiltinEntry;
+
