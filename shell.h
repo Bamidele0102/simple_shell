166bd0c07280b47464050ee3c933b3b6c92fc2e1
hegavon <victorgavon@gmail.com>
2023-11-15 09:22:29 -0800

    Lists file and header file update

--- shell.h
+++ shell.h
@@ -196,4 +196,12 @@ int read_history(ShellContext *info);
 int build_history_list(ShellContext *info, char *buf, int linecount);
 int renumber_history(ShellContext *info);
 
+/* custom_lists.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+
 #endif /* SHELL_H */
